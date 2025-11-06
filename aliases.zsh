27cc40ca1a3e4f57c1bf23406fbfe93bcaed896d
manemajef <rotem.semah@gmail.com>
2025-11-06 11:46:47 +0200

    cleanup

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,3 @@
-
 alias python="python3"
 alias pip="pip3"
 alias charm="pycharm"
@@ -6,20 +5,13 @@ alias charm="pycharm"
 alias j="zoxide query -i"
 alias zi='cd "$(zoxide query -i)"'
 alias cdi='zoxide query --interactive'
-# alias cat='bat'
-# alias z='zoxide'
-# Neovim profiles
-alias nvchad="NVIM_APPNAME=nvim.chad nvim"
-alias asvim="NVIM_APPNAME=nvim.astro nvim"
-alias lzvim="NVIM_APPNAME=nvim.lazy nvim"
-alias lz="NVIM_APPNAME=nvim.lazy nvim"
-alias kcvim="NVIM_APPNAME=nvim.kick nvim"
 alias nvconf='cd ~/.config/nvim && nvim'
 alias vim="nvim"
 alias zconf="cd && nvim ~/.zshrc"
 alias vzsh="cd ~/.zsh && nvim"
-alias cdsk='cd ~/Desktop'
-alias cdoc='cd ~/Documents'
+alias desk='~/Desktop'
+alias dcs='~/Documents'
+alias dws='~/Downloads'
 alias ghstconf='cd ~/Library/Application\ Support/com.mitchellh.ghostty && nvim config'
 alias moodle-desk='open ~/Scripts/open-moodle/dist/Moodle\ Desktop.app'
 # alias moodle='cd ~/scripts/open-moodle && /.venv/bin/python3 -m app.main'
