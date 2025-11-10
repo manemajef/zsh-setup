fd27bc99ee05699024389cd5753cc400f8fb94a8
manemajef <rotem.semah@gmail.com>
2025-11-11 01:03:44 +0200

    fix subsearch

--- antidote/antidote.zsh
+++ antidote/antidote.zsh
@@ -14,13 +14,7 @@ fi
 source "$zsh_plugins_zsh"
 
 
-# HISTORY_SUBSTRING_SEARCH_PREFIXED=1
-# HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='none'
-# HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='none'
-# bindkey '^[[A' history-substring-search-up      # Up arrow
-# bindkey '^[[B' history-substring-search-down    # Down arrow
-# bindkey '^[OA' history-substring-search-up      # Up arrow (application mode)
-# bindkey '^[OB' history-substring-search-down    # Down arrow (application mode)
+zsh-defer source "$HOME/.zsh/antidote/keybinds.zsh"
 
 ZVM_INSERT_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BEAM
 ZVM_NORMAL_MODE_CURSOR=$ZVM_CURSOR_STEADY_BLOCK
--- antidote/keybinds.zsh
+++ antidote/keybinds.zsh
@@ -1,9 +1,4 @@
 
-
-HISTORY_SUBSTRING_SEARCH_PREFIXED=1
-HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='none'
-HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='none'
-
 bindkey '^[[A' history-substring-search-up      # Up arrow
 bindkey '^[[B' history-substring-search-down    # Down arrow
 bindkey '^[OA' history-substring-search-up      # Up arrow (application mode)
