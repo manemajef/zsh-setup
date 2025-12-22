640890f6e7b776500763ff57d699db813bbf14f9
Jeff <140712389+manemajef@users.noreply.github.com>
2025-12-23 00:29:39 +0200

    yazi

---
+++ plugins/yazy.zsh
@@ -0,0 +1,8 @@
+function y() {
+  local tmp=/var/folders/_7/x3c6g_rx7qx50qgxrkzt0vl40000gn/T/yazi-cwd.XXXXXX.2iQ2kLUDBo cwd
+  yazi  --cwd-file=
+  IFS= read -r -d '' cwd < 
+  [ -n  ] && [  != /Users/rotemsemah/.zsh ] && builtin cd -- 
+  rm -f -- 
+}
+
