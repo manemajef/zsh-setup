3659a76a7c5fcd77da3ab744e8074be93b9da2bf
Jeff <140712389+manemajef@users.noreply.github.com>
2025-12-23 00:31:43 +0200

    fix yazi

--- plugins/yazy.zsh
+++ plugins/yazy.zsh
@@ -1,8 +1,8 @@
-function y() {
-  local tmp=/var/folders/_7/x3c6g_rx7qx50qgxrkzt0vl40000gn/T/yazi-cwd.XXXXXX.2iQ2kLUDBo cwd
-  yazi  --cwd-file=
-  IFS= read -r -d '' cwd < 
-  [ -n  ] && [  != /Users/rotemsemah/.zsh ] && builtin cd -- 
-  rm -f -- 
-}
 
+function y() {
+  local tmp="$(mktemp -t 'yazi-cwd.XXXXXX')" cwd
+  yazi "$@" --cwd-file="$tmp"
+  IFS= read -r -d '' cwd < "$tmp"
+  [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+  rm -f -- "$tmp"
+}
